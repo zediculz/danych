17b5676cfb18bcc1d0d6f6d254a0db7b75989838
[zedic] <oluwaseyifunmitan1@gmail.com>
2025-10-30 01:02:24 +0100

    0.1.0

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,4 +1,4 @@
-import useDanych from "../../src/index"
+import useDanych, {Danych} from "../../src/index"
 
 interface Todo {
   id: number
@@ -13,10 +13,15 @@ const db = useDanych.init<Todo>({
 
 //db.set({ id: 1, text: "some data" })
 
-const datas = db.get(0)
+const datas = db.get()
 console.log(datas)
 
 //db.update({ id: 64, text: "PIG_WAR" }, 0)
 //db.remove(0)
 
-console.log(db)
+const d = new Danych<Todo>({
+  key: "hjhjhjh",
+  type: "local"
+})
+
+console.log(d)
--- src/index.ts
+++ src/index.ts
@@ -51,9 +51,8 @@ class DanychMachine {
     }
 }
 
-
-/**@class Danych DB. */
-class Danych<T = any[]> {
+/**@class Danych is a 1kb lightweight database in the browser built on top of localStorage and sessionStorage. @param config.  @example const db = new Danych<DbType>({config}) and use the return db to get db.get(), db.get(0), set db.set(data), db.set(data, id), update db.update(newdata), db.update(newdata, id), and remove db.remove(id).. */
+export class Danych<T = any[]> {
     private config: DanychConfig
     items: T[]
     constructor(config: DanychConfig) {
@@ -151,6 +150,6 @@ class Danych<T = any[]> {
     }
 }
 
-/**@hook useDanych is Danych entry point. Danych is a 1kb lightweight database in the browser built on top of localStorage and sessionStorage. @example call useDanych.init(), const db = useDanych.init<YourDBType> and use the return db to get db.get(), db.get(0), set db.set(data), db.set(data, id), update db.update(newdata), db.update(newdata, id), and remove db.remove(id). */
+/**@hook useDanych Danych is a 1kb lightweight database in the browser built on top of localStorage and sessionStorage. @example call useDanych.init(), const db = useDanych.init<YourDBType> and use the return db to get db.get(), db.get(0), set db.set(data), db.set(data, id), update db.update(newdata), db.update(newdata, id), and remove db.remove(id). */
 const useDanych = new DanychMachine()
 export default useDanych
